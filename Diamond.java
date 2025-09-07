1ef1afa5fa9fabdfb47b5f9db33bbae8ec1a4982
Omarasif21 <omar786asif@gmail.com>
2025-09-07 00:10:15 -0700

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,18 @@
+/**
+ * Prints a diamond shape using asterisks.
+ *
+ * @author Your Name
+ * @version 1.0
+ * @since 2025-09-07
+ */
+public class Diamond {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    }
+}
---
+++ GetToKnowYou.java
@@ -0,0 +1,12 @@
+/**
+ * Prints basic information about the programmer.
+ *
+ * @author Omar Asif
+ * @version 1.0
+ * @since 2025-09-07
+ */
+public class GetToKnowYou {
+    public static void main(String[] args) {
+        System.out.println("Your Name Here\nYour Major Here\nOne interesting thing or hobby here");
+    }
+}
